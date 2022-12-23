a7f6fc25ebc2afbb40381e5977c111f44ea37813
Boris <boris@borisnl.nl>
2022-12-23 10:40:04 +0100

    discord: get code and userdata

--- src/utils/auth/providers/discord.js
+++ src/utils/auth/providers/discord.js
@@ -1,5 +1,4 @@
 import config from "../../../../config.js";
-
 const discordConfig = config.auth.providers.discord;
 
 // Setup remote url
@@ -14,7 +13,42 @@ discordConfig.remoteUrl = new URL(
 	`https://discord.com/api/oauth2/authorize?${searchParams}`
 );
 
-export function Authenticate(req, res) {
-    console.log(req, res);
-    return true;
+async function ExchangeCode(code) {
+	let params = new URLSearchParams();
+	params.append("client_id", discordConfig.clientId);
+	params.append("client_secret", discordConfig.secret);
+	params.append("grant_type", "authorization_code");
+	params.append("code", code);
+	params.append("redirect_uri", `${config.siteUrl}/auth/login/discord`);
+
+	const res = await fetch("https://discord.com/api/v10/oauth2/token", {
+		method: "POST",
+		body: params,
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Accept: "application/json",
+		},
+	});
+
+	const response = await res.json();
+	return response;
+}
+
+async function GetUserData(accessData) {
+	const res = await fetch("https://discord.com/api/v10/users/@me", {
+		method: "GET",
+		headers: { authorization: `Bearer ${accessData.access_token}` },
+	});
+
+	const response = await res.json();
+	return response;
+}
+
+export async function Authenticate(req, res) {
+	const accessData = await ExchangeCode(req.query.code);
+    console.log("🚀 ~ file: discord.js:49 ~ Authenticate ~ accessData", accessData)
+    const userData = await GetUserData(accessData);
+    console.log("🚀 ~ file: discord.js:51 ~ Authenticate ~ userData", userData)
+
+	return true;
 }
